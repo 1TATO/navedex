51e963fe9c481120b34d7eb2e5a805edd42735b2
José Neto <josenetopr5@gmail.com>
2021-04-06 11:28:32 -0300

    Created Login form validation

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,15 +1,28 @@
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import Input from '../../components/Input';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Content } from './styles';
+import { useCallback } from 'react';
 
 const Login: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data)
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string().required('Email obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
